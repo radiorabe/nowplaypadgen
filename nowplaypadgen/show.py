348237ece4d31f75ba6b0905d80febb882ff2042
Christian Affolter <christian.affolter@stepping-stone.ch>
2017-09-01 16:15:11 +0200

    Refactored start and end time methods into its own class.
    This allows us to re-use the functionality also for the upcomping track
    module.

--- nowplaypadgen/show.py
+++ nowplaypadgen/show.py
@@ -1,16 +1,19 @@
 """Show module for broadcast shows"""
 
-import datetime
 import uuid
-import pytz
+import timeperiod
 
 class ShowError(Exception):
     """Show related exception"""
     pass
 
 
-class Show(object):
+class Show(timeperiod.TimePeriod):
     """Show class which represents a specific broadcast show
+
+       A show has a name, a unique identifier, an optional description and URL,
+       as well as an absolute start and end date/time. A show can be active,
+       started or ended.
 
        Example::
 
@@ -45,128 +48,8 @@ class Show(object):
 
         self.url = None #: The show's URL
 
-        self._starttime = None #: The show's start time, initially set to None
-
-        self._endtime = None #: The show's end time, initially set to None
-
-
-    @property
-    def starttime(self):
-        """Getter for starttime
-
-        :return: The start time of the show in UTC
-        :rtype: datetime.datetime
-        """
-        return self._starttime
-
-
-    @starttime.setter
-    def starttime(self, starttime):
-        """Setter for starttime which checks for a TZ aware datetime object
-
-        :param datetime.datetime starttime: The start time of the show
-        :raises ShowError: when starttime is not a datetime object or
-                           is TZ unaware (naive)
-        """
-
-        # The current TC "aware" datetime object.
-        if not isinstance(starttime, datetime.datetime):
-            raise ShowError("starttime has to be a datetime object")
-
-        if starttime.tzinfo is None or starttime.tzinfo.utcoffset(starttime) is None:
-            raise ShowError("starttime has to be a TZ aware datetime object")
-
-        # UTC will be used internally to simplify date and time
-        # arithmetic and avoid common problems with DST boundaries.
-        # See also http://pytz.sourceforge.net/
-        starttime = starttime.astimezone(pytz.utc)
-
-        if self.endtime is not None and starttime > self.endtime:
-            msg = "starttime {0} has to be < than endtime {1}"
-            raise ShowError(msg.format(starttime, self.endtime))
-
-        self._starttime = starttime
-
-
-    @property
-    def endtime(self):
-        """Getter for endtime
-
-        :return: The end time of the show in UTC
-        :rtype: datetime.datetime
-        """
-        return self._endtime
-
-
-    @endtime.setter
-    def endtime(self, endtime):
-        """Setter for endtime which checks for a TZ aware datetime object
-
-        :param datetime.datetime endtime: The end time of the show
-        :raises ShowError: when endtime is not a datetime object or
-                           is TZ unaware (naive)
-        """
-        # The current TC "aware" datetime object.
-        if not isinstance(endtime, datetime.datetime):
-            raise ShowError("endtime has to be a datetime object")
-
-        if endtime.tzinfo is None or endtime.tzinfo.utcoffset(endtime) is None:
-            raise ShowError("endtime has to be a TZ aware datetime object")
-
-        # UTC will be used internally to simplify date and time
-        # arithmetic and avoid common problems with DST boundaries.
-        # See also http://pytz.sourceforge.net/
-        endtime = endtime.astimezone(pytz.utc)
-
-        if self.starttime is not None and endtime < self.starttime:
-            msg = "endtime {0} has to be > than starttime {1}"
-            raise ShowError(msg.format(endtime, self.starttime))
-
-        self._endtime = endtime
-
-
-    def started(self):
-        """Checks if the show has started
-
-        :return: ``True`` if the show has started, otherwise ``False``
-        :rtype: bool
-        """
-        return bool(self._starttime is not None and \
-                    datetime.datetime.now(pytz.utc) >= self._starttime)
-
-
-    def ended(self):
-        """Checks if the show has ended
-
-        :return: ``True`` if the show has ended, otherwise ``False``
-        :rtype: bool
-        """
-        return bool(self._endtime is not None and \
-                    datetime.datetime.now(pytz.utc) >= self._endtime)
-
-
-    def active(self):
-        """Checks if the show is active
-
-        An active show is defined as one that has started but not ended yet.
-
-        :return: ``True`` if the show is active, otherwise ``False``
-        :rtype: bool
-        """
-        return bool(self.started() and not self.ended())
-
-
-    def get_duration(self):
-        """Get the duration (time delta) of a show
-
-        Returns the duration of a show as a :class:datetime.timedelta object.
-        Note, that the duration is stored in days, seconds and microseconds in
-        such an object.
-
-        :return: The duration (time delta) of a show
-        :rtype: datetime.timedelta
-        """
-        return self.endtime - self.starttime
+        # Call the parent's constructor
+        super(Show, self).__init__()
 
 
     def __str__(self):
--- tests/test_show.py
+++ tests/test_show.py
@@ -4,186 +4,30 @@ import os
 import sys
 import unittest
 
-import datetime
-import pytz
-
 # Load the module locally from the dev environment.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from nowplaypadgen import show # pylint: disable=wrong-import-position
+from nowplaypadgen import timeperiod # pylint: disable=wrong-import-position
 
 
 class ShowTestSuite(unittest.TestCase):
     """Show test cases."""
 
+    def setUp(self):
+        self.show_name = "My Show Name"
+        self.my_show = show.Show(self.show_name)
+
+    def test_show_supports_timeperiod(self):
+        """Test that the show is an instance of timeperiod.TimePeriod"""
+
+        # Test that the show inherits TimePeriod, otherwise the show lacks
+        # the start and end time/date methods.
+        self.assertTrue(isinstance(self.my_show, timeperiod.TimePeriod))
+
     def test_show_name_assignment(self):
         """Test the show name assignment"""
-        show_name = "My Show Name"
 
-        sho = show.Show(show_name)
-        self.assertEqual(show_name, sho.name)
-
-
-    def test_date_assignment(self):
-        """Test the time setter and getter decorators"""
-        sho = show.Show()
-
-        sho.starttime = datetime.datetime(2017, 8, 30, 21, 0, 0, 0, pytz.utc)
-        self.assertIsInstance(sho.starttime, datetime.datetime)
-
-        sho.endtime = datetime.datetime(2017, 8, 30, 22, 0, 0, 0, pytz.utc)
-        self.assertIsInstance(sho.endtime, datetime.datetime)
-
-
-    def test_date_must_be_datetime_obj(self):
-        """Test if none datetime objects will be refused"""
-        sho = show.Show()
-
-        with self.assertRaises(show.ShowError) as context_manager:
-            sho.starttime = "Not a datetime.datetime object"
-
-        self.assertEqual('starttime has to be a datetime object',
-                         context_manager.exception.message)
-
-        with self.assertRaises(show.ShowError) as context_manager:
-            sho.endtime = "Not a datetime.datetime object"
-
-        self.assertEqual('endtime has to be a datetime object',
-                         context_manager.exception.message)
-
-
-    def test_date_must_be_tz_aware(self):
-        """Test if none TZ aware datetime objects will be refused"""
-        sho = show.Show()
-
-        with self.assertRaises(show.ShowError) as context_manager:
-            sho.starttime = datetime.datetime(2017, 8, 30, 21, 0, 0, 0)
-
-        self.assertEqual('starttime has to be a TZ aware datetime object',
-                         context_manager.exception.message)
-
-        with self.assertRaises(show.ShowError) as context_manager:
-            sho.endtime = datetime.datetime(2017, 8, 30, 22, 0, 0, 0)
-
-        self.assertEqual('endtime has to be a TZ aware datetime object',
-                         context_manager.exception.message)
-
-
-    def test_start_date_utc_conversion(self):
-        """Test if datetime objects will be converted to UTC"""
-        sho = show.Show()
-
-        # Create localized start and end times
-        zone_name = 'Europe/Zurich'
-        zurich_tz = pytz.timezone(zone_name)
-        start = zurich_tz.localize(datetime.datetime(2017, 8, 30, 21, 0, 0, 0))
-        end = zurich_tz.localize(datetime.datetime(2017, 8, 30, 22, 0, 0, 0))
-        self.assertEqual(zone_name, start.tzinfo.zone)
-        self.assertEqual(zone_name, end.tzinfo.zone)
-
-        sho.starttime = start
-        sho.endtime = end
-
-        # Start and end times must be converted to UTC internally
-        self.assertEqual('UTC', sho.starttime.tzinfo.zone)
-        self.assertEqual('UTC', sho.endtime.tzinfo.zone)
-
-
-    def test_end_not_before_start_date(self):
-        """Test that an end date can't be before a start date"""
-        sho = show.Show()
-
-        te = "hello"
-
-        start = datetime.datetime(2017, 8, 30, 22, 0, 0, 0, pytz.utc)
-        end = datetime.datetime(2017, 8, 30, 21, 0, 0, 0, pytz.utc)
-
-        sho.starttime = start
-
-        with self.assertRaises(show.ShowError) as context_manager:
-            sho.endtime = end
-
-        error = "endtime {0} has to be > than starttime {1}".format(end, start)
-        self.assertEqual(error, context_manager.exception.message)
-
-
-    def test_start_after_end_date(self):
-        """Test that a start date can't be after and end date"""
-        sho = show.Show()
-
-        start = datetime.datetime(2017, 8, 30, 22, 0, 0, 0, pytz.utc)
-        end = datetime.datetime(2017, 8, 30, 21, 0, 0, 0, pytz.utc)
-
-        sho.endtime = end
-
-        with self.assertRaises(show.ShowError) as context_manager:
-            sho.starttime = start
-
-        error = "starttime {0} has to be < than endtime {1}".format(start, end)
-        self.assertEqual(error, context_manager.exception.message)
-
-
-    def test_show_has_started(self):
-        """Test that a show has started"""
-        sho = show.Show()
-        now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-
-        sho.starttime = now
-        sho.endtime = now + datetime.timedelta(hours=1) # plus one hour
-
-        self.assertTrue(sho.active())
-        self.assertTrue(sho.started())
-        self.assertFalse(sho.ended())
-
-
-    def test_show_has_not_started(self):
-        """Test that a show hasn't started yet"""
-        sho = show.Show()
-        now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-
-        sho.starttime = now + datetime.timedelta(hours=1) # plus one hour
-        sho.endtime   = now + datetime.timedelta(hours=2) # plus two hours
-        self.assertFalse(sho.active())
-        self.assertFalse(sho.started())
-        self.assertFalse(sho.ended())
-
-
-    def test_show_has_ended(self):
-        """Test that a show has ended"""
-        sho = show.Show()
-        now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-
-        sho.starttime = now - datetime.timedelta(hours=2) # minus two hours
-        sho.endtime   = now - datetime.timedelta(hours=1) # minus one hour
-        self.assertFalse(sho.active())
-        self.assertTrue(sho.started())
-        self.assertTrue(sho.ended())
-
-
-    def test_show_has_not_ended(self):
-        """Test that a show hasn't ended yet"""
-        sho = show.Show()
-        now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-
-        sho.starttime = now
-        sho.endtime   = now + datetime.timedelta(hours=1) # plus one hour
-        self.assertTrue(sho.active())
-        self.assertTrue(sho.started())
-        self.assertFalse(sho.ended())
-
-
-    def test_show_duration(self):
-        """Test the duration (time delta) of a show"""
-        sho = show.Show()
-        now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
-
-        # The show runs for one hour
-        sho.starttime = now
-        sho.endtime   = now + datetime.timedelta(hours=1)
-
-        expected_delta = datetime.timedelta(0,3600)
-
-        self.assertEqual(sho.get_duration(), expected_delta)
-
+        self.assertEqual(self.show_name, self.my_show.name)
 
 
 if __name__ == '__main__':
