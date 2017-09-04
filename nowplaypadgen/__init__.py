62b2a7f1bf3bc3438a5bcc76f71937749b828eec
Christian Affolter <christian.affolter@stepping-stone.ch>
2017-09-04 19:49:17 +0200

    Specify modules for wildcard import.

--- nowplaypadgen/__init__.py
+++ nowplaypadgen/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 
+# Prevent "TypeError: Item in ``from list'' not a string" due to
+# unicode_literals
+__all__ = [str('show'), str('timeperiod')]
 __version__ = '0.1.0'
+
 
 class Error(Exception):
     """Base class for exceptions in this module."""
