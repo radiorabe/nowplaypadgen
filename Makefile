9f971a62e0d93dcccc2cb1c17d52357998e36fd0
Lucas Bickel <hairmare@purplehaze.ch>
2021-12-25 12:35:01 +0100

    feat: release pipeline for tagging and publishing
    * add semantic releasing based on conventional-commit and go-semantic-release
    * add publishing to pypi using twine
    * move cli interface to nowplaypadgenctl module
    * do not publish nowplaypadgenctl cli module (it's wip)

--- Makefile
+++ Makefile
@@ -14,6 +14,14 @@ docs: api-doc ## Generate documentation.
 api-doc: ## Generate API docs using Sphinx.
 	sphinx-apidoc -M -f -o docs/api nowplaypadgen
 
+.PHONY: clean
+clean:  ## Clean dist
+	rm -rf dist/ build/ .eggs/
+
+.PHONY: dist
+dist: clean ## Build dist
+	python setup.py sdist bdist_wheel
+
 .PHONY: help
 help: ## Display this help.
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- setup.py
+++ setup.py
@@ -1,40 +1,25 @@
 """Set up nowplaypadgen."""
 
-import os
-import re
-
 from setuptools import find_packages, setup
 
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 
-def get_version():
-    """Get version from main module."""
-    version_file = os.path.join("nowplaypadgen", "__init__.py")
-    initfile_lines = open(version_file, "rt", encoding="utf-8").readlines()
-    for line in initfile_lines:
-        matches = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", line, re.M)
-        if matches:
-            return matches.group(1)
-    raise RuntimeError(f"Unable to find version string in {version_file}.")
-
-
 setup(
     name="nowplaypadgen",
-    version=get_version(),
     description="DAB+ now playing PAD (DLS+ and MOT SLS generator)",
     url="https://github.com/radiorabe/nowplaypadgen",
-    author="Christian Affolter",
-    author_email="c.affolter@purplehaze.ch",
+    author="RaBe IT-Reaktion",
+    author_email="it@rabe.ch",
     license="AGPL-3",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
+    version_config={"starting_version": "0.1.0"},
+    setup_requires=["setuptools-git-versioning"],
     install_requires=requirements,
-    include_package_data=True,
-    entry_points={"console_scripts": ["nowplay-padgen = nowplaypadgen.__main__:main"]},
-    zip_safe=False,
+    entry_points={"console_scripts": ["nowplaypadgenctl = nowplaypadgenctl:main"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: AGPL License",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
     ],
 )
